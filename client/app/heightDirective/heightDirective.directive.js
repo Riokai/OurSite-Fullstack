43d6275d827ce1a6684c0c7359466c7aa4ef19b5
Kai <zengkai2009@foxmail.com>
2015-08-09 15:44:30 +0800

    add height directive

---
+++ client/app/heightDirective/heightDirective.directive.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('ourSiteApp')
+  .directive('heightDirective', function () {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        // element.text('this is the heightDirective directive');
+        var width = document.documentElement.clientWidth;
+        var height = document.documentElement.clientHeight;
+
+        element.css({
+        	// width: width,
+        	minHeight: height
+        });
+      }
+    };
+  });
