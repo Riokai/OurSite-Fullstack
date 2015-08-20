e6b5bf6b7561bf4408bb4dd705bc13d9c0309e26
Kai <zengkai2009@foxmail.com>
2015-08-20 23:16:52 +0800

    add server message auth feature

--- client/app/app.js
+++ client/app/app.js
@@ -30,7 +30,8 @@ angular.module('ourSiteApp', [
       // Intercept 401s and redirect you to login
       responseError: function(response) {
         if(response.status === 401) {
-          $location.path('/login');
+          $location.path('/');
+          alert('请先登录！');
           // remove any stale tokens
           $cookieStore.remove('token');
           return $q.reject(response);
